83066bad09bbd746719d37f9e1b3b36b0d42ffdd
NormalOne <149543061+Fedir-M@users.noreply.github.com>
2024-02-18 13:08:11 -0500

    hw2_feedback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,53 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import Section from './Section';
+import FeedbackOptions from './FeedbackOptions';
+
+import s from './App.module.css';
+import Statistics from './Statistics';
+import Notification from './Notification';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onButtonClick = state => {
+    this.setState(prevState => ({ [state]: prevState[state] + 1 }));
+  };
+
+  countTotalFeedback = () => {
+    const {good, neutral, bad} = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state;
+    const total = this.countTotalFeedback();
+    return Math.round((good / total) * 100) || 0;
+  };
+
+  render() {
+    const totalClicks = this.countTotalFeedback();
+
+    return (
+      <div className={s.app}>
+        <Section title="Please Leave Feedback">
+          <FeedbackOptions onButtonClick={this.onButtonClick} option={Object.keys(this.state)}/>
+        </Section>
+        <Section title="Statistic">
+          {totalClicks > 0 ?
+          <Statistics
+            good={this.state.good}
+            neutral={this.state.neutral}
+            bad={this.state.bad}
+            total={this.countTotalFeedback()}
+            positivePercentage={this.countPositiveFeedbackPercentage()}
+          />
+          : <Notification message="There is no feedback" />}
+        </Section>
+      </div>
+    );
+  }
+}
---
+++ src/components/Notification/Notification.jsx
@@ -0,0 +1,9 @@
+import s from './Notification.module.css';
+
+const Notification = ({message}) => {
+  return (
+    <strong>{message}</strong>
+  )
+}
+
+export default Notification
